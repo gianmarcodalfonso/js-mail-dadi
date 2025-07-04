4c79966eae9b885df25066eb2d5470fb24450220
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-04 15:20:35 +0200

    feat: script.js invitedList array & for(instruction)

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const invitedList = [`giorgio@rndm.com`,`maria@rndm.com`,`pippo@rndm.com`,`paperino@rndm.com`,`pietro@rndm.com`,`francesca@rndm.com`]
+console.log(`${invitedList}`)
+
+for(let i = 0; i < invitedList.lenght; i++){
+
+}
